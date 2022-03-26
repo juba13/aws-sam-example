65c8722b1226a55a0539e78364081582b51f2d9c
Md Jubayer <mohammed.jubayer@gmail.com>
2022-03-27 01:38:49 +0600

    refactor response service

--- src/service/response.service.mjs
+++ src/service/response.service.mjs
@@ -1,7 +1,12 @@
 import {StatusCodes} from 'http-status-codes';
+
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Credentials': true
+}
+
 class AppResponse{
     constructor() {
-      super();
     }
     static create = () => { 
       return new AppResponse();
@@ -16,8 +21,49 @@ class AppResponse{
       this.statusCode= statusCode;
       return this;
     } 
-    static ok = (body) => { return AppResponse.create().addBody(body).addStatusCode(StatusCodes.OK)}
-    static error = (body,code) => { return AppResponse.create().addBody(body).addStatusCode(code?.startsWith("2"))? StatusCodes.INTERNAL_SERVER_ERROR : code}
+
+
+    static ok = (data) => {
+      const response = {
+        statusCode: 200,
+        headers: {
+          ...corsHeaders
+        },
+        body: JSON.stringify({
+          ok: true,
+          data
+        })
+      }
+      return response
+    }
+  
+    static error = (error, statusCode ) => {
+      if (!statusCode) statusCode = error.statusCode || 500
+  
+      if (error.length) {
+        // handle batch of errors
+      }
+  
+      const message = typeof error === 'string'
+        ? error
+        : error.message
+  
+      const response = {
+        statusCode,
+        headers: {
+          ...corsHeaders
+        },
+        body: JSON.stringify({
+          ok: false,
+          status: statusCode,
+          error: message,
+          data: typeof error === 'object' ? error?.data : {}
+        })
+      }
+      return response
+    }
+
+
 }
 
 export { AppResponse as response }
